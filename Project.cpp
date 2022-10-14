371b5bc3b1b4b5d65de991f80e9ab8879492aa1f
聖 横山 <kijitora-39@outlook.jp>
2022-10-14 10:04:17 +0900

    Visual Stdioで

---
+++ Project.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine;
+
+int main() { 
+std::cout<<"Hello VisualStdio Git!\n";
+std::cout<<"Hello New branch\n";
+}
